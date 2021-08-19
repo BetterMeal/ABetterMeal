7d434e56aad27aa09c4982c56d8481b47b31865a
BetterMeal <89132454+BetterMeal@users.noreply.github.com>
2021-08-19 22:03:41 +0800

    Update timelock.js

--- js/timelock.js
+++ js/timelock.js
@@ -1,1 +1,1 @@
-new Date().getHours() >= 9 && new Date().getHours() <= 12 ? document.getElementById("home").href="./service.html" : document.getElementById("home").href="https://usfns.tk/";
+new Date().getHours() >= 9 && new Date().getHours() <= 11 ? document.getElementById("home").href="./service.html" : document.getElementById("home").href="https://usfns.tk/";

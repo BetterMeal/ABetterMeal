ad6270b00c286ecbe9e4a21a9f7b010df3886aab
xiakele <xiakele123@gmail.com>
2021-08-20 19:47:42 +0800

    restored "index()"

--- js/print-dev.js
+++ js/print-dev.js
@@ -1,5 +1,5 @@
 "use strict";
-// function index() {
+function index() {
 let day = ["", "周一", "周二", "周三", "周四", "周五", "周六", "周日"];
 let date = `${new Date().getMonth() + 1}/${new Date().getDate()}&nbsp${day[new Date().getDay()]}`;
 let areaNumber = ["", "自助餐", "套餐A", "套餐A", "套餐B", "套餐B", "盖浇饭", "套餐C", "面档", "套餐B"];
@@ -21,6 +21,6 @@ if (area >= 1 && area <= 3) {
     document.getElementById("floor-print").innerHTML = "二";
 }
 document.getElementById("district-print").innerHTML = areaNumber[area];
-// }
-// new Date().getHours() >= 9 && new Date().getHours() <= 12 ? index() : window.location.replace("https://usfns.tk/");
+}
+new Date().getHours() >= 9 && new Date().getHours() <= 12 ? index() : window.location.replace("https://usfns.tk/");
 // index();

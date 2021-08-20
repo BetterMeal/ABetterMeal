918e0ddba70735efc41110f832a5643171350dfa
BetterMeal <89132454+BetterMeal@users.noreply.github.com>
2021-08-20 10:40:38 +0800

    Add files via upload

--- js/timelock.js
+++ js/timelock.js
@@ -1,4 +1,1 @@
-new Date().getHours() >= 9 && new Date().getHours() <= 11 ? document.getElementById("home").href="./service.html" : document.getElementById("home").href="https://usfns.tk/";
-document.getElementById("home").addEventListener("dblclick",function(){
-	window.location.replace("./service.html");
-})
+new Date().getHours() >= 9 && new Date().getHours() <= 11 ? document.getElementById("home").addEventListener("dblclick",function(){window.location.replace("./service.html");}) : document.getElementById("home").href="https://usfns.tk/";

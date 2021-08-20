cc0bc9d672b5bb167e315cf6072f6fadbde13c74
BetterMeal <89132454+BetterMeal@users.noreply.github.com>
2021-08-20 10:33:28 +0800

    Add files via upload

--- js/timelock.js
+++ js/timelock.js
@@ -1,1 +1,4 @@
 new Date().getHours() >= 9 && new Date().getHours() <= 11 ? document.getElementById("home").href="./service.html" : document.getElementById("home").href="https://usfns.tk/";
+document.getElementById("home").addEventListener("dblclick",function(){
+	window.location.replace("./service.html");
+})

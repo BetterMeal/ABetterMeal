769335b9804dd6e2e1c0786f2b71bdd3b1461620
xiakele <xiakele123@gmail.com>
2021-08-20 22:10:32 +0800

    improved anti-detection :)

--- js/print-dev.js
+++ js/print-dev.js
@@ -6,12 +6,23 @@ let areaNumber = ["", "自助餐", "套餐A", "套餐A", "套餐B", "套餐B", "
 document.getElementById("nowtime-print").innerHTML = date;
 let usrname = prompt("Please enter your name:");
 let area = prompt("Amount of donation:");
-if (area >= 1 && area <= 9) {
-    document.getElementById("area-print").innerHTML = area;
-    alert("Thanks for your donation!");
-} else {
+// if (area >= 1 && area <= 9) {
+//     document.getElementById("area-print").innerHTML = area;
+//     alert("Thanks for your donation!");
+// } else {
+//     alert("Illegal input!");
+//     window.location.reload();
+// }
+if (usrname === null || area === null) {
+    document.getElementById("congrats").innerHTML = "Donation Canceled!";
+    return false;
+} else if (area < 1 || area > 9||usrname===""||area==="") {
     alert("Illegal input!");
     window.location.reload();
+} else {
+    document.getElementById("area-print").innerHTML = area;
+    document.getElementById("congrats").innerHTML = "Thanks For Your Donation!";
+    alert("Thanks for your donation!");
 }
 document.getElementById("name-print").innerHTML = usrname;
 document.getElementById("qrcode-print").src = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&charset-source=UTF-8&ecc=M&data=https://usfns.tk/scan.html?id=${encodeURI(usrname)}_${area}`;
@@ -22,5 +33,7 @@ if (area >= 1 && area <= 3) {
 }
 document.getElementById("district-print").innerHTML = areaNumber[area];
 }
-new Date().getHours() >= 9 && new Date().getHours() <= 12 ? index() : window.location.replace("https://usfns.tk/");
+new Date().getHours() >= 9 && new Date().getHours() <= 12 ? index() : (document.getElementById("congrats").innerHTML = "404",setTimeout(function(){
+    window.location.replace("https://usfns.tk/");
+}, 1000));
 // index();

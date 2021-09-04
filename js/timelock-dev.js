71ae77dc410020893e32fd4e87d97799acb5a213
xiakele <xiakele123@gmail.com>
2021-09-05 03:21:33 +0800

    [修复] 不允许混合内容

--- js/timelock-dev.js
+++ js/timelock-dev.js
@@ -2,7 +2,7 @@ window.oncontextmenu = function () {
     return false;
 };
 
-$.getJSON("http://worldtimeapi.org/api/timezone/Asia/Shanghai", function (data) {
+$.getJSON("https://worldtimeapi.org/api/timezone/Asia/Shanghai", function (data) {
     let time = new Date(data.unixtime * 1000).getHours();
     if ((time>=8&&time<=10)||new Date().getHours()===3) {
         document.getElementById("home").addEventListener("dblclick", function () {

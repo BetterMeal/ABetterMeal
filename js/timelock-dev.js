139fdcbb4b18c140a6c0840bd63c292fa632914b
xiakele <xiakele123@gmail.com>
2021-10-16 11:36:11 +0800

    [功能] 原生js实现api调用

--- js/timelock-dev.js
+++ js/timelock-dev.js
@@ -2,11 +2,22 @@ window.oncontextmenu = function () {
     return false;
 };
 
-$.getJSON("https://worldtimeapi.org/api/timezone/Asia/Shanghai", function (data) {
-    let time = new Date(data.unixtime * 1000).getHours();
-    if ((time>=8&&time<=10)||new Date().getHours()===3) {
-        document.getElementById("home").addEventListener("dblclick", function () {
-            window.location.replace("https://w2m.usfns.tk/donation.html?track_id=af609ed206722bd0793713d50ca2858e897633b75d0453f97b6ab61c42c2e7e6");
-        });
-    }
-});
+// $.getJSON("https://worldtimeapi.org/api/timezone/Asia/Shanghai", function (data) {
+//     let time = new Date(data.unixtime * 1000).getHours();
+//     if ((time>=8&&time<=10)||new Date().getHours()===3) {
+//         document.getElementById("home").addEventListener("dblclick", function () {
+//             window.location.replace("https://w2m.usfns.tk/donation.html?track_id=af609ed206722bd0793713d50ca2858e897633b75d0453f97b6ab61c42c2e7e6");
+//         });
+//     }
+// });
+
+fetch("https://worldtimeapi.org/api/timezone/Asia/Shanghai")
+    .then(response => response.json())
+    .then((json) => {
+        let time = (new Date(json.unixtime * 1000)).getHours();
+        if ((time >= 8 && time <= 10) || new Date().getHours() === 3) {
+            document.getElementById("home").addEventListener("dblclick", function () {
+                window.location.replace("https://w2m.usfns.tk/donation.html?track_id=af609ed206722bd0793713d50ca2858e897633b75d0453f97b6ab61c42c2e7e6");
+            });
+        }
+    });
